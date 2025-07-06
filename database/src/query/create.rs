16a0b28f57b0ba7e860c67083a26ee3a8e7149f6
supertypo <suprtypo@pm.me>
2025-07-07 01:19:57 +0200

    Dont attempt CREATE TABLE if it exists. Fixed Docker build.

--- database/src/query/create.rs
+++ database/src/query/create.rs
@@ -1,27 +1,63 @@
 use sqlx::{Error, Pool, Postgres};
 
 pub async fn create_tables(pool: &Pool<Postgres>) -> Result<(), Error> {
-    sqlx::query(
-        "CREATE TABLE IF NOT EXISTS distribution_tiers (
-            timestamp BIGINT,
-            tier SMALLINT,
-            count BIGINT,
-            amount BIGINT,
-            PRIMARY KEY (timestamp, tier)
+    create_distribution_tiers(pool).await?;
+    create_top_scripts(pool).await?;
+    Ok(())
+}
+
+async fn create_distribution_tiers(pool: &Pool<Postgres>) -> Result<(), Error> {
+    let exists: bool = sqlx::query_scalar(
+        "SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            AND table_name = 'distribution_tiers'
         )",
     )
-    .execute(pool)
+    .fetch_one(pool)
     .await?;
-    sqlx::query(
-        "CREATE TABLE IF NOT EXISTS top_scripts (
-            timestamp BIGINT,
-            rank SMALLINT,
-            script_public_key BYTEA,
-            amount BIGINT,
-            PRIMARY KEY (timestamp, rank)
+
+    if !exists {
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS distribution_tiers (
+                timestamp BIGINT,
+                tier SMALLINT,
+                count BIGINT,
+                amount BIGINT,
+                PRIMARY KEY (timestamp, tier)
+            )",
+        )
+        .execute(pool)
+        .await?;
+    }
+    Ok(())
+}
+
+async fn create_top_scripts(pool: &Pool<Postgres>) -> Result<(), Error> {
+    let exists: bool = sqlx::query_scalar(
+        "SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            AND table_name = 'top_scripts'
         )",
     )
-    .execute(pool)
+    .fetch_one(pool)
     .await?;
+
+    if !exists {
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS top_scripts (
+                timestamp BIGINT,
+                rank SMALLINT,
+                script_public_key BYTEA,
+                amount BIGINT,
+                PRIMARY KEY (timestamp, rank)
+            )",
+        )
+        .execute(pool)
+        .await?;
+    }
     Ok(())
 }
